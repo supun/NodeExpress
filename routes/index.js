c0adb9c6855429bc2c655f74a8c56f9187d5651a
Supun dharmarathna <supun921@gmail.com>
2013-12-26 15:12:35 +0530

    db connection added

--- routes/index.js
+++ routes/index.js
@@ -10,3 +10,14 @@ exports.index = function(req, res){
 exports.helloworld = function(req,res){
     res.render('helloworld', { title: 'Hello, World!' });
 };
+
+exports.userlist = function(db) {
+    return function(req, res) {
+        var collection = db.get('usercollection');
+        collection.find({},{},function(e,docs){
+            res.render('userlist', {
+                "userlist" : docs
+            });
+        });
+    };
+};
